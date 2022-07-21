f11db3b62f1e244cea0ea918805f90a8ef1e706b
Eliot Makoski <eliot@makoski.net>
2022-07-22 00:13:45 +1000

    path is relative

--- src/vendors/firebase/index.js
+++ src/vendors/firebase/index.js
@@ -2,7 +2,7 @@ const Firestore = require("@google-cloud/firestore");
 
 const db = new Firestore({
   projectId: "homehub-c2f7c",
-  keyFilename: "firebase_keys.json",
+  keyFilename: "./firebase_keys.json",
 });
 
 export { db };
