1deb9deb7d0f0879a4219985571a650fbe1a2ab7
saketpani <saketpani@gmail.com>
2022-08-08 23:43:46 +0530

    updated the UI

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,6 +1,8 @@
 const TaskItem = ({title}) => {
     return (<div className="flex mb-4 items-center">
-                <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" id="inlineCheckbox1" value="option1" />
+                <input 
+                className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" 
+                type="checkbox" />
                 <p className="w-full text-grey-darkest">{title}</p>                
                 <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red">Remove</button>
             </div>);
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -9,11 +9,11 @@ const TaskList = ({tasks}) => {
         <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
             {titleLayout}
             <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
-                <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
-                    <div className="mb-4">
-                        <h1 className="text-grey-darkest">Task List</h1>
-                        <div className="flex mt-4">
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add Todo" />
+                <div className="bg-white rounded shadow p-6 w-full">
+                    <div className="mb-2">                        
+                        <div className="flex">
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-1 text-grey-darker" 
+                                   placeholder="Add Task" />
                             <button className="flex-no-shrink p-2 border-2 rounded text-teal border-teal">+</button>
                         </div>
                     </div>                        
