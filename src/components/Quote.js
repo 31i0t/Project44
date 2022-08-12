de85bec5f3dc94cb587cf32ca4acb50d3d47fa69
saketpani <saketpani@gmail.com>
2022-08-12 21:44:06 +0530

    Renamed the quote component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,6 +1,7 @@
 // assets quote reference : https://www.fishbowlinventory.com/blog/2013/04/01/8-great-inventory-management-quotes/
+// https://animoto.com/blog/business/inspirational-quotes-real-estate-business
 
-const quotes = [
+const quotesData = [
     {
         quote: "One of the great responsibilities that I have is to manage my assets wisely, so that they create value.",
         author: "Alice Walton"
@@ -12,12 +13,16 @@ const quotes = [
     {
         quote: "If you count all your assets you always show a profit.",
         author: "Wilson Mizner"
+    },
+    {
+        quote: "Home is the nicest word there is.",
+        author: "Laura Ingalls Wilder"
     }
 ];
 
-const AssetQuote = () => {
+const Quote = () => {
     
-    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    const randomQuote = quotesData[Math.floor(Math.random() * quotesData.length)];
 
     return (
         <div className="main-banner p-4">
@@ -26,4 +31,4 @@ const AssetQuote = () => {
     );
 }
 
-export default AssetQuote;
+export default Quote;
