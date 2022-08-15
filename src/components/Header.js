f0cf4844219b4ec9ea071f097c883903a7f86c29
saketpani <saketpani@gmail.com>
2022-08-15 23:32:56 +0530

    Updated Header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ const Header = ({title, summary}) => {
     return (
         <div className="w-full bg-cover bg-center">
             <div className="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50 header">
-                <div className="text-center">
-                    <h1 className="text-white p-2 text-2xl font-semibold uppercase md:text-l">
+                <div className="text-center p-2">
+                    <h1 className="text-white text-2xl font-semibold uppercase md:text-l">
                         {title}
                     </h1>
-                    <h4 className="text-white p-2 font-semibold md:text-l">{summary}</h4>
+                    <h4 className="text-white font-semibold md:text-l">{summary}</h4>
                 </div>
             </div>
         </div>
