a865380b58527d5fd2889165337f2e3688a0fb90
saketpani <saketpani@gmail.com>
2022-08-15 23:31:12 +0530

    header style moved to global css.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,7 @@
-const Header = ({title, summary}) => {
-    const style = {
-        width: "100%",
-        height: "80px"        
-    };
-
+const Header = ({title, summary}) => {   
     return (
         <div className="w-full bg-cover bg-center">
-            <div style={style}
-                className="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
+            <div className="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50 header">
                 <div className="text-center">
                     <h1 className="text-white p-2 text-2xl font-semibold uppercase md:text-l">
                         {title}
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -24,7 +24,7 @@ const TaskList = ({tasks, addTask}) => {
                 </div>
             </div>
             <div className="flex flex-col gap-3">
-                {tasks.map(item => TaskItem(item))}
+                {tasks.map(item => <TaskItem key={item.title} title={item.title} /> )}
             </div>
         </div>
     );
