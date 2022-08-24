813aa376ad7d6374e4d22b84e8ff80c4a2890365
eliot <eliot@flexa.co>
2022-08-24 21:11:53 +1000

    fixed BASE_URL

--- src/pages/api/rooms/delete.js
+++ src/pages/api/rooms/delete.js
@@ -1,11 +1,11 @@
 import { db } from "../../../vendors/firebase";
 
-const deleteInventory = (id) => db.collection("inventory").doc(id.toString()).delete();
+const deleteInventory = (id) => db.collection("inventory").doc(id).delete();
 
 export default async function handler(req, res) {
   try {
     const { id } = req.body;
-    var roomRef = await db.collection("rooms").doc(id.toString());
+    var roomRef = await db.collection("rooms").doc(id);
     var roomData = await roomRef.get().then((r) => r.data());
     // delete inventory related to this room
     await Promise.all(roomData.inventory.map(deleteInventory));
--- src/pages/api/tasks/delete.js
+++ src/pages/api/tasks/delete.js
@@ -3,7 +3,7 @@ import { db } from "../../../vendors/firebase";
 export default async function handler(req, res) {
   try {
     const { id } = req.body;
-    await db.collection("tasks").doc(id.toString()).delete();
+    await db.collection("tasks").doc(id).delete();
     res.status(200).json({ message: 'Deleted successfully'});
   }
   catch (error) {
